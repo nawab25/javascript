1193dde8dfedcfa96134fd4e1751f59dd43c869d
gmnawab <gmnawab25@gmail.com>
2023-03-01 13:05:09 +0600

    js added

---
+++ app.js
@@ -0,0 +1,57 @@
+
+// var num1 = "60";
+// var num2 = 40;
+// num1 = parseFloat(num1);
+
+// var total = num1 + num2
+// console.log(total)
+
+
+// var num1 = 30;
+// var num2 = "40";
+// var num3 = 30;
+// num2 = parseInt(num2);
+
+// nawab = num1+num2+num3
+// console.log(nawab)
+
+// var num1 = 40;
+// var num2 = 30;
+// // num2 = "" + num2
+// var total = num1 + num2;
+// console.log(total);
+
+// var abc = 30;
+// var cde = "40";
+// var fgh = 50.8848;
+// cde = parseInt(cde);
+// var total = abc+cde+fgh;
+// console.log(total);
+
+
+// var num1 = 100;
+// var num2 = 60;
+// var total = num1 + num2;
+// console.log(total);
+
+
+// var num1 = 40;
+// var num2 = 20;
+// num2 = "" + num2;
+// console.log(typeof num2);
+
+// var num1 = 90;
+// var num2 = 20;
+// var total = num1 % num2;
+// console.log(total);
+
+// var num1 = -40;
+// var sum = Math.abs(num1);
+// console.log(sum);
+
+// var num1 = 4.9991;
+// var nawab = Math.floor(num1);
+// console.log(nawab);
+
+var nawab = Math.random() * 100;
+console.log(nawab);
